4f0fa5d50cc3f2163681abb57ea61593ea20ab5b
Ophelie <ophelie.lhermitte@gmail.com>
2026-03-02 20:50:08 +0100

    test Mailgun

--- config.nodemailer.js
+++ config.nodemailer.js
@@ -2,19 +2,19 @@ import nodemailer from "nodemailer";
 import mg from "nodemailer-mailgun-transport";
 
 export const sendEmail = async ({ firstName, lastName, email, message }) => {
+
   const auth = {
     auth: {
       api_key: process.env.MAILGUN_API_KEY,
       domain: process.env.MAILGUN_DOMAIN,
     },
-    host: "api.eu.mailgun.net",
   };
 
   const transporter = nodemailer.createTransport(mg(auth));
 
   const mailOptions = {
-    from: "Mailgun Sandbox <postmaster@sandbox30d30147c7974b8dbf05ac93866aec50.mailgun.org>",
-    to: "ophelie.lhermitte@gmail.com",
+    from: `postmaster@${process.env.MAILGUN_DOMAIN}`,
+    to: process.env.MAILGUN_TO,
     subject: `Nouveau message depuis le portfolio de : ${firstName} ${lastName} (${email})`,
     text: `Nom: ${firstName} ${lastName}\nEmail: ${email}\nMessage: ${message}`,
     html: `<p><strong>Nom :</strong> ${firstName} ${lastName}</p>
@@ -29,7 +29,6 @@ export const sendEmail = async ({ firstName, lastName, email, message }) => {
     return info;
   } catch (error) {
     console.error("Erreur Mailgun :", error);
-    throw error;
   }
 };
 
--- index.js
+++ index.js
@@ -4,18 +4,6 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import routesNodemailer from "./routes.nodemailer.js";
 
-import nodemailer from "nodemailer";
-import mg from "nodemailer-mailgun-transport";
-
-const auth = {
-  auth: {
-    api_key: process.env.MAILGUN_API_KEY,
-    domain: process.env.MAILGUN_DOMAIN,
-  },
-};
-
-const transporter = nodemailer.createTransport(mg(auth));
-
 const app = express();
 
 app.set("trust proxy", 1);
@@ -36,23 +24,6 @@ app.use("/api", routesNodemailer);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server running");
-});
-
-app.get("/api/test-mail", async (req, res) => {
-  try {
-    const info = await transporter.sendMail({
-      from: `postmaster@${process.env.MAILGUN_DOMAIN}`,
-      to: "ophelie.lhermitte@gmail.com",
-      subject: "Test Mailgun",
-      text: "Hello world",
-    });
-
-    console.log("Mail envoyé:", info);
-    res.status(200).json({ success: true });
-  } catch (error) {
-    console.error("Erreur test mail:", error);
-    res.status(500).json({ error: error.message });
-  }
 });
 
 export default app;
